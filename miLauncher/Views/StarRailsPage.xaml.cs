75aada5b7fdf51eef7b8345867fb10eaa4dc8a76
Crataletio <531016334@qq.com>
2023-11-28 15:33:53 +0800

    update pickfile

--- miLauncher/Views/StarRailsPage.xaml.cs
+++ miLauncher/Views/StarRailsPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 using miLauncher.ViewModels;
+using Windows.Storage.Pickers;
 
 namespace miLauncher.Views;
 
@@ -15,5 +17,37 @@ public sealed partial class StarRailsPage : Page
     {
         ViewModel = App.GetService<StarRailsViewModel>();
         InitializeComponent();
+    }
+
+    private async void PickAFileButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Clear previous returned file name, if it exists, between iterations of this scenario
+        // OutputTextBlock.Text = "";
+
+        // Create a file picker
+        var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+        // Retrieve the window handle (HWND) of the current WinUI 3 window.
+        //TODO: Repeair it by Finding WindowsHelper.cs on WinUI3 source code
+        // var window = WindowHelper.GetWindowForElement(this);
+        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+
+        // Initialize the file picker with the window handle (HWND).
+        WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+        // Set options for your file picker
+        openPicker.ViewMode = PickerViewMode.Thumbnail;
+        openPicker.FileTypeFilter.Add("*");
+
+        // Open the picker for the user to pick a file
+        var file = await openPicker.PickSingleFileAsync();
+        if (file != null)
+        {
+            PickAFileOutputTextBlock.Text = "Picked file: " + file.Name;
+        }
+        else
+        {
+            PickAFileOutputTextBlock.Text = "Operation cancelled.";
+        }
     }
 }
